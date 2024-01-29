9911ad05de14a6de75c44abd370ed948aaafd289
AlexF23 <152330796+AlexF23@users.noreply.github.com>
2024-01-29 16:25:34 +0000

    Initial commit

---
+++ src/ca1_alexander_fay_sba22368/CA1_Alexander_Fay_sba22368.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ca1_alexander_fay_sba22368;
+
+/**
+ *
+ * @author Alexander
+ */
+public class CA1_Alexander_Fay_sba22368 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
